fe0e3ca030a5cde36559e272718a0c8f4b4e88ee
Matthew Daly <matthewbdaly@gmail.com>
2018-04-17 21:16:16 +0100

    Installed Twig support

--- src/dependencies.php
+++ src/dependencies.php
@@ -23,3 +23,16 @@ $container['logger'] = function ($c) {
 $container['parser'] = function ($c) {
     return new Parser;
 };
+
+// Use Twig for views
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('path/to/templates', [
+        'cache' => 'path/to/cache'
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
